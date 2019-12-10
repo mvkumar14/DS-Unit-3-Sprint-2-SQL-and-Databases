3ede620e14ce8546a1d303fdf4bbccc02339b3fa
Kumar Veeravel <mvkumar14@gmail.com>
2019-12-09 16:58:47 -0800

    finished 1/3 of the assignment.

---
+++ module1-introduction-to-sql/in_class_ex.py
@@ -0,0 +1,45 @@
+import sqlite3
+import os
+import time
+
+#Creating a connection object
+conn = sqlite3.connect('toy_data.db')
+
+#Be able to see the directory from within the python file
+print(os.listdir())
+
+# writing out a command for the sql db that we created
+query = 'CREATE TABLE toy (name varchar(30),size int);'
+
+# creating an object to interact with db
+curs = conn.cursor()
+print(dir(curs))
+
+# executing the query
+curs.execute(query)
+
+# closing the cursor, and commiting the changes.
+curs.close()
+conn.commit()
+
+# opening new cursor, and trying to access informtion from the db
+curs2 = conn.cursor()
+curs2.execute('SELECT * FROM toy;').fetchall()
+
+# adding a value to the table
+insert_query = "INSERT INTO toy (name,size) VALUES ('AWESOME',27);"
+
+# closing and saving the cursor
+curs2.execute(insert_query)
+curs2.close()
+conn.commit()
+
+# opening third cursor, and checking that the changes saved
+curs3 = conn.cursor()
+print(curs3.execute('SELECT * FROM  toy;').fetchall())
+curs3.close()
+conn.commit()
+
+conn.close()
+# time.sleep(1)
+os.remove('toy_data.db')
---
+++ module1-introduction-to-sql/rpg_queries.py
@@ -0,0 +1,57 @@
+import os
+import sqlite3
+
+
+
+# this only works when the cursor is curs, but that can be made
+# an input variable to make the function general.
+def amt(query):
+    return len(curs.execute(query).fetchall())
+
+conn = sqlite3.connect("rpg_db.sqlite3")
+query = 'SELECT * FROM charactercreator_character;'
+
+
+curs = conn.cursor()
+# 1 Total characters:
+print("Total characters:", len(curs.execute(query).fetchall()),'\n')
+
+# 2 Number of each subclass:
+char_list = ['mage','necromancer','thief','cleric','fighter']
+
+for i in char_list:
+    my_query = 'SELECT * FROM charactercreator_' + i + ';'
+    print(i.upper(),":", amt(my_query))
+
+# 3 Number of items
+item_query = "SELECT * FROM armory_item"
+print("\nTotal items:", amt(item_query))
+
+# 4 Number of Weapon items
+weapon_items = 'SELECT * FROM armory_weapon'
+print('\nTotal weapons:', amt(weapon_items))
+
+# 5 Number of non-weapon items
+print("\nTotal non-weapons", amt(item_query)-amt(weapon_items))
+
+# 6 How many items does each character have.
+# The following is the naive way to get the information needed:
+for i in range(1,20):
+    num_items = f'SELECT * FROM charactercreator_character_inventory WHERE' +
+        f'character_id IS {i}'
+    print(amt(num_items))
+
+# Here is a better way to get the information... but I don't understand this
+# as well.
+count_items = 'SELECT character_id,count(character_id)' +
+    'FROM charactercreator_character_inventory GROUP BY character_id'
+print(curs.execute(count_items).fetchall())
+
+# You will notice that the second value in the tuples matches
+# the values in the first output
+
+# for i in range(1,20):
+#     num_items = f'SELECT * FROM charactercreator_character_inventory WHERE character_id IS {i}'
+#     sub = curs.execute()
+#to get the names of the tables
+# SELECT name FROM sqlite_master WHERE type = 'table'
